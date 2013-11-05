03b04b37a6a771f37ff466757be7bf04da6c959d
perykou <root@pitnet.com.es>
2013-11-05 15:03:00 -0500

    Genera un archivo de entrada con los magnets a abrir

--- insert.sh
+++ insert.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+#mata aria2c, para que el archivo insert.sh (se ejecuta cada minuto) vuelva a abrirlo con el parametro de entrada el archivo generado "direcc"
 pkill aria2c
 wget -O /home/archivos/rtorrent/torrents/rss.xml 'http://showrss.info/rss.php?user_id=152366&hd=null&proper=null'
 xmllint --xpath ' //item/link/text()' /home/archivos/rtorrent/torrents/rss.xml > /home/archivos/rtorrent/torrents/direcc
-#sed -i '/'magnet'/'prrrrrr/g' /home/archivos/rtorrent/torrents/direcc
-#cadena= cat "/home/archivos/rtorrent/torrents/direcc"
 cc=`cat /home/archivos/rtorrent/torrents/direcc`
 echo -e  ${cc//magnet:?xt/"\r\nmagnet:?xt"} > /home/archivos/rtorrent/torrents/direcc
 
