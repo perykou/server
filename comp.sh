e21675e872a29504597a6141ab65406602e72fe3
perykou <root@pitnet.com.es>
2013-11-05 15:08:45 -0500

    Archivo que se abre cada minuto con crontab

--- comp.sh
+++ comp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#Este archivo comprueba que aria2c está abierto y si no te crea un demonio 
+#con escuchando por rpc para el acceso porv con la webUI
 ps -e | grep aria2c
 if [ $? -ne 0 ]; then
 screen -dmS aria aria2c --conf-path=/root/.aria2/aria2.conf --enable-rpc --rpc-listen-all
